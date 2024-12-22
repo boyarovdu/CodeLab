1f1718c31c8f30685e235d35e8034f26f610ca26
David Boiarov <boyarovdu@gmail.com>
2024-12-22 15:21:59 +0200

    First rough iteration of RedBlackTree implementation, C#/F#

---
+++ Algorithms.RedBlackTree/Algorithms.RedBlackTree/RedBlackTree.cs
@@ -0,0 +1,384 @@
+﻿using System;
+
+namespace Algorithms.RedBlackTree
+{
+    public enum NodeColor
+    {
+        Red,
+        Black
+    }
+
+    public class RedBlackTreeNode<T> where T : IComparable
+    {
+        public T Value { get; set; }
+        public NodeColor Color { get; set; }
+        public RedBlackTreeNode<T> Left { get; set; }
+        public RedBlackTreeNode<T> Right { get; set; }
+        public RedBlackTreeNode<T> Parent { get; set; }
+
+        public RedBlackTreeNode(T value)
+        {
+            Value = value;
+            Color = NodeColor.Red;
+            Left = null;
+            Right = null;
+            Parent = null;
+        }
+    }
+
+    public class RedBlackTree<T> where T : IComparable
+    {
+        private RedBlackTreeNode<T> _root;
+        private readonly RedBlackTreeNode<T> _nullNode;
+
+        public RedBlackTree()
+        {
+            _nullNode = new RedBlackTreeNode<T>(default(T))
+            {
+                Color = NodeColor.Black
+            };
+            _root = _nullNode;
+        }
+
+        public void Insert(T value)
+        {
+            var newNode = new RedBlackTreeNode<T>(value)
+            {
+                Left = _nullNode,
+                Right = _nullNode,
+                Parent = null
+            };
+
+            if (_root == _nullNode)
+            {
+                _root = newNode;
+                _root.Color = NodeColor.Black;
+                return;
+            }
+
+            var current = _root;
+            RedBlackTreeNode<T> parent = null;
+
+            while (current != _nullNode)
+            {
+                parent = current;
+                if (newNode.Value.CompareTo(current.Value) < 0)
+                {
+                    current = current.Left;
+                }
+                else
+                {
+                    current = current.Right;
+                }
+            }
+
+            newNode.Parent = parent;
+            if (newNode.Value.CompareTo(parent.Value) < 0)
+            {
+                parent.Left = newNode;
+            }
+            else
+            {
+                parent.Right = newNode;
+            }
+
+            FixInsert(newNode);
+        }
+
+        private void FixInsert(RedBlackTreeNode<T> node)
+        {
+            while (node != _root && node.Parent.Color == NodeColor.Red) // 1, 2
+            {
+                if (node.Parent == node.Parent.Parent.Left) // 3
+                {
+                    var uncle = node.Parent.Parent.Right;
+                    if (uncle.Color == NodeColor.Red) // 3.1
+                    {
+                        node.Parent.Color = NodeColor.Black;
+                        uncle.Color = NodeColor.Black;
+                        node.Parent.Parent.Color = NodeColor.Red;
+                        node = node.Parent.Parent;
+                    }
+                    else // 3.2
+                    {
+                        if (node == node.Parent.Right) // 3.2.1
+                        {
+                            node = node.Parent;
+                            LeftRotate(node);
+                        }
+
+                        // 3.2.2
+                        node.Parent.Color = NodeColor.Black;
+                        node.Parent.Parent.Color = NodeColor.Red;
+                        RightRotate(node.Parent.Parent);
+                    }
+                }
+                else // 4 
+                {
+                    var uncle = node.Parent.Parent.Left;
+                    if (uncle.Color == NodeColor.Red)
+                    {
+                        // Case 1: Uncle is red
+                        node.Parent.Color = NodeColor.Black;
+                        uncle.Color = NodeColor.Black;
+                        node.Parent.Parent.Color = NodeColor.Red;
+                        node = node.Parent.Parent;
+                    }
+                    else
+                    {
+                        if (node == node.Parent.Left)
+                        {
+                            // Case 2: Uncle is black, node is left child
+                            node = node.Parent;
+                            RightRotate(node);
+                        }
+
+                        // Case 3: Uncle is black, node is right child
+                        node.Parent.Color = NodeColor.Black;
+                        node.Parent.Parent.Color = NodeColor.Red;
+                        LeftRotate(node.Parent.Parent);
+                    }
+                }
+            }
+
+            _root.Color = NodeColor.Black;
+        }
+
+        public void Delete(T value)
+        {
+            var nodeToDelete = Search(value);
+            if (nodeToDelete == _nullNode) return;
+
+            var y = nodeToDelete;
+            var yOriginalColor = y.Color;
+            RedBlackTreeNode<T> x;
+
+            if (nodeToDelete.Left == _nullNode)
+            {
+                x = nodeToDelete.Right;
+                Transplant(nodeToDelete, nodeToDelete.Right);
+            }
+            else if (nodeToDelete.Right == _nullNode)
+            {
+                x = nodeToDelete.Left;
+                Transplant(nodeToDelete, nodeToDelete.Left);
+            }
+            else
+            {
+                y = Minimum(nodeToDelete.Right);
+                yOriginalColor = y.Color;
+                x = y.Right;
+                if (y.Parent == nodeToDelete)
+                {
+                    x.Parent = y;
+                }
+                else
+                {
+                    Transplant(y, y.Right);
+                    y.Right = nodeToDelete.Right;
+                    y.Right.Parent = y;
+                }
+
+                Transplant(nodeToDelete, y);
+                y.Left = nodeToDelete.Left;
+                y.Left.Parent = y;
+                y.Color = nodeToDelete.Color;
+            }
+
+            if (yOriginalColor == NodeColor.Black)
+            {
+                FixDelete(x);
+            }
+        }
+
+        private void FixDelete(RedBlackTreeNode<T> node)
+        {
+            while (node != _root && node.Color == NodeColor.Black)
+            {
+                if (node == node.Parent.Left)
+                {
+                    var sibling = node.Parent.Right;
+                    if (sibling.Color == NodeColor.Red)
+                    {
+                        sibling.Color = NodeColor.Black;
+                        node.Parent.Color = NodeColor.Red;
+                        LeftRotate(node.Parent);
+                        sibling = node.Parent.Right;
+                    }
+
+                    if (sibling.Left.Color == NodeColor.Black && sibling.Right.Color == NodeColor.Black)
+                    {
+                        sibling.Color = NodeColor.Red;
+                        node = node.Parent;
+                    }
+                    else
+                    {
+                        if (sibling.Right.Color == NodeColor.Black)
+                        {
+                            sibling.Left.Color = NodeColor.Black;
+                            sibling.Color = NodeColor.Red;
+                            RightRotate(sibling);
+                            sibling = node.Parent.Right;
+                        }
+
+                        sibling.Color = node.Parent.Color;
+                        node.Parent.Color = NodeColor.Black;
+                        sibling.Right.Color = NodeColor.Black;
+                        LeftRotate(node.Parent);
+                        node = _root;
+                    }
+                }
+                else
+                {
+                    var sibling = node.Parent.Left;
+                    if (sibling.Color == NodeColor.Red)
+                    {
+                        sibling.Color = NodeColor.Black;
+                        node.Parent.Color = NodeColor.Red;
+                        RightRotate(node.Parent);
+                        sibling = node.Parent.Left;
+                    }
+
+                    if (sibling.Left.Color == NodeColor.Black && sibling.Right.Color == NodeColor.Black)
+                    {
+                        sibling.Color = NodeColor.Red;
+                        node = node.Parent;
+                    }
+                    else
+                    {
+                        if (sibling.Left.Color == NodeColor.Black)
+                        {
+                            sibling.Right.Color = NodeColor.Black;
+                            sibling.Color = NodeColor.Red;
+                            LeftRotate(sibling);
+                            sibling = node.Parent.Left;
+                        }
+
+                        sibling.Color = node.Parent.Color;
+                        node.Parent.Color = NodeColor.Black;
+                        sibling.Left.Color = NodeColor.Black;
+                        RightRotate(node.Parent);
+                        node = _root;
+                    }
+                }
+            }
+
+            node.Color = NodeColor.Black;
+        }
+
+        private void Transplant(RedBlackTreeNode<T> u, RedBlackTreeNode<T> v)
+        {
+            if (u.Parent == null)
+            {
+                _root = v;
+            }
+            else if (u == u.Parent.Left)
+            {
+                u.Parent.Left = v;
+            }
+            else
+            {
+                u.Parent.Right = v;
+            }
+
+            v.Parent = u.Parent;
+        }
+
+        private RedBlackTreeNode<T> Minimum(RedBlackTreeNode<T> node)
+        {
+            while (node.Left != _nullNode)
+            {
+                node = node.Left;
+            }
+
+            return node;
+        }
+
+        public RedBlackTreeNode<T> Search(T value)
+        {
+            var current = _root;
+            while (current != _nullNode)
+            {
+                if (value.CompareTo(current.Value) == 0)
+                {
+                    return current;
+                }
+                current = value.CompareTo(current.Value) < 0 ? current.Left : current.Right;
+            }
+
+            return _nullNode;
+        }
+
+        public void InOrderTraversal(Action<T> action)
+        {
+            InOrderTraversal(_root, action);
+        }
+
+        private void InOrderTraversal(RedBlackTreeNode<T> node, Action<T> action)
+        {
+            if (node == null || node == _nullNode) return;
+
+            InOrderTraversal(node.Left, action);
+            action(node.Value);
+            InOrderTraversal(node.Right, action);
+        }
+
+        private void LeftRotate(RedBlackTreeNode<T> node)
+        {
+            var temp = node.Right;
+            node.Right = temp.Left;
+
+            if (temp.Left != _nullNode)
+            {
+                temp.Left.Parent = node;
+            }
+
+            temp.Parent = node.Parent;
+
+            if (node.Parent == null)
+            {
+                _root = temp;
+            }
+            else if (node == node.Parent.Left)
+            {
+                node.Parent.Left = temp;
+            }
+            else
+            {
+                node.Parent.Right = temp;
+            }
+
+            temp.Left = node;
+            node.Parent = temp;
+        }
+
+        private void RightRotate(RedBlackTreeNode<T> node)
+        {
+            var temp = node.Left;
+            node.Left = temp.Right;
+
+            if (temp.Right != _nullNode)
+            {
+                temp.Right.Parent = node;
+            }
+
+            temp.Parent = node.Parent;
+
+            if (node.Parent == null)
+            {
+                _root = temp;
+            }
+            else if (node == node.Parent.Right)
+            {
+                node.Parent.Right = temp;
+            }
+            else
+            {
+                node.Parent.Left = temp;
+            }
+
+            temp.Right = node;
+            node.Parent = temp;
+        }
+    }
+}
---
+++ Algorithms.RedBlackTree/Algorithms.RedBlackTree_FSharp/RedBlackTree.fs
@@ -0,0 +1,116 @@
+﻿module RedBlackTree_FSharp.RedBlackTree
+
+
+type Color =
+    | Red
+    | Black
+
+
+type Tree<'T> =
+    | Empty
+    | Node of Color * Tree<'T> * 'T * Tree<'T>
+
+
+// Monad-like helper functions for operations
+let todo value = (true, value)
+let done' value = (false, value)
+let fromResult result = snd result
+
+let xxx f x =
+    let (isTodo, value) = x
+    (isTodo, f value)
+
+
+let (=<<) handler x =
+    let (isTodo, value) = x
+    if isTodo then handler value else (isTodo, value)
+
+
+let blacken tree =
+    match tree with
+    | Node(Red, left, x, right) -> Node(Black, left, x, right)
+    | _ -> tree
+
+
+let insert tree x =
+    let rec ins t =
+        match t with
+        | Empty -> todo (Node(Red, Empty, x, Empty))
+        | Node(color, left, y, right) ->
+            if x < y then
+                (=<<) balance (xxx (fun l -> Node(color, l, y, right)) (ins left))
+            elif x > y then
+                (=<<) balance (xxx (fun r -> Node(color, left, y, r)) (ins right))
+            else
+                done' t
+
+    and balance t =
+        match t with
+        | Node(Black, Node(Red, Node(Red, a, x, b), y, c), z, d) // Case 1
+        | Node(Black, Node(Red, a, x, Node(Red, b, y, c)), z, d) // Case 2
+        | Node(Black, a, x, Node(Red, Node(Red, b, y, c), z, d)) // Case 3
+        | Node(Black, a, x, Node(Red, b, y, Node(Red, c, z, d))) -> // Case 4
+            todo (Node(Red, Node(Black, a, x, b), y, Node(Black, c, z, d)))
+        | _ -> done' t
+
+    blacken (fromResult (ins tree))
+
+
+let rec delete tree x =
+    let rec del t =
+        match t with
+        | Empty -> done' Empty
+        | Node(color, left, y, right) ->
+            if x < y then
+                (=<<) delLeft (xxx (fun l -> Node(color, l, y, right)) (del left))
+            elif x > y then
+                (=<<) delRight (xxx (fun r -> Node(color, left, y, r)) (del right))
+            else
+                delRoot t
+
+    and delRoot t =
+        match t with
+        | Node(Black, left, _, Empty) -> blacken left |> done'
+        | Node(Red, left, _, Empty) -> done' left
+        | Node(color, left, _, right) ->
+            let m = ref None // Mutable reference to hold the min value
+            (=<<) delRight (xxx (fun r -> Node(color, left, (m.Value).Value, r)) (delMin right m))
+
+    and delMin t m =
+        match t with
+        | Node(Black, Empty, y, right) ->
+            m.Value <- Some y
+            blacken right |> done'
+        | Node(Red, Empty, y, right) ->
+            m.Value <- Some y
+            done' right
+        | Node(color, left, y, right) -> (=<<) delLeft (xxx (fun l -> Node(color, l, y, right)) (delMin left m))
+
+    and delLeft t =
+        match t with
+        | Node(Black, Node(Red, left, x, right), y, sibling) ->
+            todo (Node(Red, Node(Black, left, x, right), y, sibling))
+        | Node(color, left, y, sibling) -> balance (Node(color, left, y, sibling)) |> done'
+        | _ -> done' t
+
+    and delRight t =
+        match t with
+        | Node(Black, sibling, y, Node(Red, left, x, right)) ->
+            todo (Node(Red, sibling, y, Node(Black, left, x, right)))
+        | Node(color, sibling, y, right) -> balance (Node(color, sibling, y, right)) |> done'
+        | _ -> done' t
+
+    and balance t =
+        let (xxx, node) =
+
+            match t with
+            | Node(color, Node(Red, Node(Red, a, x, b), y, c), z, d) // Symmetric balancing cases
+            | Node(color, Node(Red, a, x, Node(Red, b, y, c)), z, d)
+            | Node(color, a, x, Node(Red, Node(Red, b, y, c), z, d))
+            | Node(color, a, x, Node(Red, b, y, Node(Red, c, z, d))) ->
+                done' (Node(color, Node(Black, a, x, b), y, Node(Black, c, z, d)))
+            | _ -> blacken t |> done'
+
+        node
+
+    blacken (fromResult (del tree))
