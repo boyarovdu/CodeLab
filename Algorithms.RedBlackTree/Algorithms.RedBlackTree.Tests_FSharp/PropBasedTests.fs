022f9b39c677f90323b1fe4dcae25ea3674835b5
David <boyarovdu@gmail.com>
2024-12-26 19:04:08 +0200

    Remove obsolete RedBlackTree_Original and update test setup
    The redundant `RedBlackTree_Original.fs` file was deleted along with its project reference. Updated test properties, cleaned up module and function definitions, and refined package references in the test project for better compatibility and maintainability.

--- Algorithms.RedBlackTree/Algorithms.RedBlackTree.Tests_FSharp/PropBasedTests.fs
+++ Algorithms.RedBlackTree/Algorithms.RedBlackTree.Tests_FSharp/PropBasedTests.fs
@@ -1,104 +1,97 @@
-namespace RedBlackTree_FSharp.Tests
+module RedBlackTreePropsTests
 
+open FsCheck
 open FsCheck.NUnit
 open RedBlackTree_FSharp.RedBlackTree
 
-type PropBasedTests() =
+let isRootBlack tree =
+    match tree with
+    | Nil -> true
+    | Node(color, _, _, _) -> color = Black
 
-    let isRootBlack tree =
-        match tree with
-        | Nil -> true
-        | Node(color, _, _, _) -> color = Black
+let rec noRedRedViolation tree =
+    match tree with
+    | Nil -> true
+    | Node(Red, Node(Red, _, _, _), _, _) -> false
+    | Node(Red, _, _, Node(Red, _, _, _)) -> false
+    | Node(_, left, _, right) -> noRedRedViolation left && noRedRedViolation right
 
-    let rec noRedRedViolation tree =
-        match tree with
-        | Nil -> true
-        | Node(Red, Node(Red, _, _, _), _, _) -> false
-        | Node(Red, _, _, Node(Red, _, _, _)) -> false
-        | Node(_, left, _, right) -> noRedRedViolation left && noRedRedViolation right
+let rec blackHeightValid tree : (bool * int) =
+    match tree with
+    | Nil -> true, 1
+    | Node(color, left, _, right) ->
+        let (lValid, lHeight) = blackHeightValid left
+        let (rValid, rHeight) = blackHeightValid right
 
-    let rec blackHeightValid tree : (bool * int) =
-        match tree with
-        | Nil -> true, 1
-        | Node(color, left, _, right) ->
-            let (lValid, lHeight) = blackHeightValid left
-            let (rValid, rHeight) = blackHeightValid right
+        lValid && rValid && lHeight = rHeight, (if color = Black then lHeight + 1 else lHeight)
 
-            lValid && rValid && lHeight = rHeight, (if color = Black then lHeight + 1 else lHeight)
+let rec isBST (tree: Tree<'T>) =
 
-    let rec isBST (tree: Tree<'T>) =
+    match tree with
+    | Nil -> true
+    | Node(_, left, y, right) ->
+        match left, right with
+        | Nil, Nil -> true
+        | Node(_, _, x, _), Node(_, _, z, _) -> x < y && y < z
+        | Nil, Node(_, _, z, _) -> y < z
+        | Node(_, _, x, _), Nil -> x < y
+        && isBST left
+        && isBST right
 
-        match tree with
-        | Nil -> true
-        | Node(_, left, y, right) ->
-            match left, right with
-            | Nil, Nil -> true
-            | Node(_, _, x, _), Node(_, _, z, _) -> x < y && y < z
-            | Nil, Node(_, _, z, _) -> y < z
-            | Node(_, _, x, _), Nil -> x < y
-            && isBST left
-            && isBST right
+[<Property>]
+let ``Root is always black after insert``(values: int list) =
+    let res = List.fold insert Nil values |> isRootBlack
+    NUnit.Framework.Assert.IsTrue(res)
 
+[<Property>]
+let ``Red nodes cannot have red children after insert``(values: int list) =
+    List.fold insert Nil values |> noRedRedViolation
 
-    [<Property>]
-    member _.``Root is always black after insert``(values: int list) =
-        // Insert values and validate root property
-        let tree = List.fold insert Nil values
-        isRootBlack tree
+[<Property>]
+let ``Black height is consistent across all paths after insert``(values: int list) =
+    List.fold insert Nil values |> blackHeightValid |> fst
 
-    [<Property>]
-    member _.``Red nodes cannot have red children after insert``(values: int list) =
-        List.fold insert Nil values |> noRedRedViolation
+[<Property>]
+let ``Tree satisfies the BST property after insert``(values: int list) =
+    List.fold insert Nil values |> isBST
 
-    [<Property>]
-    member _.``Black height is consistent across all paths after insert``(values: int list) =
-        List.fold insert Nil values |> blackHeightValid |> fst
+[<Property>]
+let ``Root is always black after delete``(values: int list) =
+    if values.Length > 0
+    then List.randomChoice values |> delete(List.fold insert Nil values) |> isRootBlack
+    else true
 
-    [<Property>]
-    member _.``Tree satisfies the BST property after insert``(values: int list) =
-        List.fold insert Nil values |> isBST
+[<Property>]
+let ``Red nodes cannot have red children after delete``(values: int list) =
+    if values.Length > 0
+    then List.randomChoice values |> delete (List.fold insert Nil values) |> noRedRedViolation
+    else true
 
-    [<Property>]
-    member _.``Root is always black after delete``(values: int list) =
-        if values.Length > 0
-        then List.randomChoice values |> delete(List.fold insert Nil values) |> isRootBlack
-        else true
+[<Property>]
+let ``Black height is consistent across all paths after delete``(values: int list) =
+    if values.Length > 0
+    then List.randomChoice values |> delete (List.fold insert Nil values) |> blackHeightValid |> fst
+    else true
 
-    [<Property>]
-    member _.``Red nodes cannot have red children after delete``(values: int list) =
-        if values.Length > 0
-        then List.randomChoice values |> delete (List.fold insert Nil values) |> noRedRedViolation
-        else true
+[<Property>]
+let ``Tree satisfies the BST property after delete``(values: int list) =
+    if values.Length > 0
+    then List.randomChoice values |> delete (List.fold insert Nil values) |> isBST
+    else true
 
-    [<Property>]
-    member _.``Black height is consistent across all paths after delete``(values: int list) =
-        if values.Length > 0
-        then List.randomChoice values |> delete (List.fold insert Nil values) |> blackHeightValid |> fst
-        else true
+[<Property>]
+let ``Root is always black after random value delete`` (values: int list) (valToDelete: int) =
+    valToDelete |> delete(List.fold insert Nil values) |> isRootBlack
 
+[<Property>]
+let ``Red nodes cannot have red children after random value delete`` (values: int list) (valToDelete: int) =
+    valToDelete |> delete (List.fold insert Nil values) |> noRedRedViolation
 
-    [<Property>]
-    member _.``Tree satisfies the BST property after delete``(values: int list) =
-        if values.Length > 0
-        then List.randomChoice values |> delete (List.fold insert Nil values) |> isBST
-        else true
+[<Property>]
+let ``Black height is consistent across all paths after random val delete``
+    (values: int list)(valToDelete: int) =
+    valToDelete |> delete (List.fold insert Nil values) |> blackHeightValid |> fst
 
-    [<Property>]
-    member _.``Root is always black after random value delete`` (values: int list) (valToDelete: int) =
-        valToDelete |> delete(List.fold insert Nil values) |> isRootBlack
-
-
-    [<Property>]
-    member _.``Red nodes cannot have red children after random value delete`` (values: int list) (valToDelete: int) =
-        valToDelete |> delete (List.fold insert Nil values) |> noRedRedViolation
-
-    [<Property>]
-    member _.``Black height is consistent across all paths after random val delete``
-        (values: int list)
-        (valToDelete: int)
-        =
-        valToDelete |> delete (List.fold insert Nil values) |> blackHeightValid |> fst
-
-    [<Property>]
-    member _.``Tree satisfies the BST property after random value delete``(values: int list)(valToDelete: int) =
-        valToDelete |> delete (List.fold insert Nil values) |> isBST
+[<Property>]
+let ``Tree satisfies the BST property after random value delete``(values: int list)(valToDelete: int) =
+    valToDelete |> delete (List.fold insert Nil values) |> isBST
