6b8d7c1efc34d43ab1c8c840bd94b3528c296918
David Boiarov <boyarovdu@gmail.com>
2025-01-07 15:39:10 +0200

    Fixing potential issue that may appear in environments where processor count is not power of two
    Introduced `Util.ToNearestLowerPowerOfTwo()` to determine the maximum degree of parallelism based on the nearest lower power of two of processor count. Updated `MergeSortParallel` to use this value for partitioning and merging

--- MergeSort/Algorithms.MergeSort/MergeSortParallel.cs
+++ MergeSort/Algorithms.MergeSort/MergeSortParallel.cs
@@ -8,58 +8,58 @@ public struct Partition
 
 public static class MergeSortParallel
 {
-    private static int ProcessorCount => Environment.ProcessorCount;
-
     public static void Sort(int[] array)
     {
+        var degreeOfParallelism = Util.ToNearestLowerPowerOfTwo(Environment.ProcessorCount);
+
         var memory = array.AsMemory();
         var buffer = new int[memory.Length].AsMemory();
 
-        Sort(memory, buffer);
+        Sort(memory, buffer, degreeOfParallelism);
     }
 
-    private static void Sort(Memory<int> memory, Memory<int> buffer)
+    private static void Sort(Memory<int> memory, Memory<int> buffer, int degreeOfParallelism)
     {
-        var partitions = Partitioner.CalculatePartitions(memory.Length, ProcessorCount);
+        var partitions = Partitioner.CalculatePartitions(memory.Length, degreeOfParallelism);
 
         Parallel.ForEach(partitions,
-            new ParallelOptions { MaxDegreeOfParallelism = ProcessorCount },
+            new ParallelOptions { MaxDegreeOfParallelism = degreeOfParallelism },
             partition =>
                 MergeSortGcFriendly.Sort(
                     memory.Slice(partition.Offset, partition.Length).Span,
                     buffer.Slice(partition.Offset, partition.Length).Span));
 
-        Merge(memory, buffer, partitions);
+        Merge(memory, buffer, partitions, degreeOfParallelism);
     }
 
-    private static void Merge(Memory<int> memory, Memory<int> buffer, Partition[] partitions, int mergeFactor = 2)
+    private static void Merge(Memory<int> memory, Memory<int> buffer, Partition[] partitions, int degreeOfParallelism,
+        int mergeFactor = 2)
     {
         var numberOfMerges = partitions.Length / mergeFactor;
 
         if (numberOfMerges <= 0) return;
 
-        Parallel.For(0, numberOfMerges, new ParallelOptions { MaxDegreeOfParallelism = ProcessorCount },
+        Parallel.For(0, numberOfMerges, new ParallelOptions { MaxDegreeOfParallelism = degreeOfParallelism },
             operationIndex =>
             {
-                var leftPartitionIndex = operationIndex * mergeFactor;
-                var rightPartitionIndex = leftPartitionIndex + mergeFactor / 2;
+                var leftIndex = operationIndex * mergeFactor;
+                var rightIndex = leftIndex + mergeFactor / 2;
 
-                var leftOffset = partitions[leftPartitionIndex].Offset;
-                var rightOffset = partitions[rightPartitionIndex].Offset;
+                var leftOffset = partitions[leftIndex].Offset;
+                var rightOffset = partitions[rightIndex].Offset;
 
                 var leftLength = rightOffset - leftOffset;
                 var rightLength = operationIndex == numberOfMerges - 1
                     ? memory.Length - rightOffset
-                    : partitions[rightPartitionIndex + mergeFactor / 2].Offset - rightOffset;
+                    : partitions[rightIndex + mergeFactor / 2].Offset - rightOffset;
 
-                var sliceLeft = memory.Slice(leftOffset, leftLength);
-                var sliceRight = memory.Slice(rightOffset, rightLength);
-                var sliceBuffer = buffer.Slice(leftOffset, leftLength + rightLength);
-
-                MergeSortGcFriendly.Merge(sliceLeft.Span, sliceRight.Span, sliceBuffer.Span);
+                MergeSortGcFriendly.Merge(
+                    memory.Slice(leftOffset, leftLength).Span,
+                    memory.Slice(rightOffset, rightLength).Span,
+                    buffer.Slice(leftOffset, leftLength + rightLength).Span);
             });
 
         buffer.CopyTo(memory);
-        Merge(memory, buffer, partitions, mergeFactor * 2);
+        Merge(memory, buffer, partitions, degreeOfParallelism, mergeFactor * 2);
     }
 }
---
+++ MergeSort/Algorithms.MergeSort/Util.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithms.MergeSort;
+
+internal static class Util
+{
+    internal static int ToNearestLowerPowerOfTwo(int number)
+    {
+        if ((number & (number - 1)) == 0) 
+            return number;
+
+        number |= (number >> 1);
+        number |= (number >> 2);
+        number |= (number >> 4);
+        number |= (number >> 8);
+        number |= (number >> 16);
+
+        return number - (number >> 1);
+    }
+}
