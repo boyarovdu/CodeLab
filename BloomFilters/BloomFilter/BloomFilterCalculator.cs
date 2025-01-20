610fc79613be9dd8b4b85275868b0c4c1e536502
boyarovdu <boyarovdu@gmail.com>
2025-01-20 20:43:32 +0200

    Feature/bloom filter (#4)
    * Initial implementation of bloom filter
    
    * Sln name for BloomFilter proj has been changed
    
    * Fixed typo and added comment to BloomFilterCalculator referencing the source
    
    * Small renaming for better readability
    
    * Removing magic number
    
    * Fix output formatting and correct typo in Console messages
    
    * Update console log messages for clarity and consistency

---
+++ BloomFilters/BloomFilter/BloomFilterCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace BloomFilter;
+
+using System;
+
+// For more information check paper called "Space/Time Trade-offs in Hash Coding with Allowable Errors" by BURTON H. BLOOM 
+static class BloomFilterCalculator
+{
+    private static double Log2(double x) => Math.Log(x) / Math.Log(2.0);
+
+    public static double CalculateTotalBitsNumber(int numberOfItems, double errorFraction, double zeroBitsFraction) =>
+        numberOfItems * (-1 * Log2(errorFraction)) * (Log2(Math.E) / Log2(zeroBitsFraction) * Log2(1.0 - zeroBitsFraction));
+    
+    public static double CalculateBitsNumberPerItem(int numberOfItems, double zeroBitsFraction, double totalBitsNumber) =>
+        Log2(zeroBitsFraction) *  totalBitsNumber / -numberOfItems * Log2(Math.E);
+}
