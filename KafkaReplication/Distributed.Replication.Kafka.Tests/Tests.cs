70d8ecdcbfd8acbfcebef9b505378d7d293430eb
david <boyarovdu@gmail.com>
2025-02-04 23:51:42 +0100

    Add test-web-client service and setup in tests
    Introduce a `test-web-client` service in `docker-compose` and configure it in test setup. This includes building and running the service with Kafka producer settings to support test execution.

--- KafkaReplication/Distributed.Replication.Kafka.Tests/Tests.cs
+++ KafkaReplication/Distributed.Replication.Kafka.Tests/Tests.cs
@@ -7,6 +7,18 @@ public class Tests : BaseDockerTest
 {
     protected override string DockerComposeFolderPath => "../../../../";
 
+    [OneTimeSetUp]
+#pragma warning disable CS0108, CS0114
+    public async Task StartDockerCompose()
+#pragma warning restore CS0108, CS0114
+    {
+        // This command builds 
+        // docker run --name consumer-1 -p 5117:8080  distributed.replication.kafka.testwebclient dotnet Distributed.Replication.Kafka.TestWebClient.dll --kafka-config bootstrap.servers=kafka-1:9092,kafka-2:9092,kafka-3:9092 --client-type producer
+        // await Cmd.ExecAsync(DockerComposeFolderPath, "docker", "build -t distributed.replication.kafka.testwebclient -f Distributed.Replication.Kafka.TestWebClient/Dockerfile .");
+        
+        await Cmd.ExecAsync(DockerComposeFolderPath, "docker", "run -d --name consumer-1 -p 5117:8080  test-web-client dotnet Distributed.Replication.Kafka.TestWebClient.dll --kafka-config bootstrap.servers=kafka-1:9092,kafka-2:9092,kafka-3:9092 --client-type producer");
+    }
+    
     [Test]
     public async Task TestA()
     {
